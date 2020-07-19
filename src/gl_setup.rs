dea22cf8de0eb5aeb4803cfe4b1a1ef1d0861ee8
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-07-19 13:19:25 +0300

    Implement a better way to get canvas info on Rust

--- src/gl_setup.rs
+++ src/gl_setup.rs
@@ -1,15 +1,27 @@
 use wasm_bindgen::JsCast;
 use wasm_bindgen::JsValue;
 use web_sys::*;
-use web_sys::WebGlRenderingContext as GL;
+use web_sys::{HtmlCanvasElement, Document, WebGlRenderingContext as GL};
 use crate::utils::console_log;
+
+
+fn wait_until_canvas_is_rendered(document: Document) -> Result<HtmlCanvasElement, Element> {
+    let canvas: Option<Element> = document.get_element_by_id("canvasRust");
+    match canvas {
+        Some(elem) => {
+            elem.dyn_into::<web_sys::HtmlCanvasElement>()
+        }
+        None => {
+            return wait_until_canvas_is_rendered(document);
+        }
+    }
+}
 
 pub fn initialize_webgl_context() -> Result<WebGlRenderingContext, JsValue> {
     console_log("Inicializing webgl");
     let window = window().unwrap();
     let document = window.document().unwrap();
-    let canvas = document.get_element_by_id("canvasRust").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+    let canvas: HtmlCanvasElement = wait_until_canvas_is_rendered(document)?;
     let gl: WebGlRenderingContext = canvas.get_context("webgl")?.unwrap().dyn_into()?;
 
     gl.clear_color(0.0, 0.0, 0.0, 1.0); //RGBA
